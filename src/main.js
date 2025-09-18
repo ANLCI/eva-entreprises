7761836ae268aae667a93697a21f030b132bde4f
Clément Prod'homme <clement.prod-homme@captive.fr>
2025-09-18 11:58:18 +0200

    first commit

---
+++ src/main.js
@@ -0,0 +1,22 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import '@gouvfr/dsfr/dist/dsfr.min.css'      // Import des styles du DSFR //
+import '@gouvminint/vue-dsfr/styles'         // Import des styles globaux propre à VueDSFR //
+import VueDsfr from '@gouvminint/vue-dsfr'   // Import (par défaut) de la bibliothèque //
+import { VueQueryPlugin } from '@tanstack/vue-query'
+
+
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(VueDsfr)
+app.use(VueQueryPlugin)
+
+app.mount('#app')
---
+++ src/views/FormView.vue
@@ -0,0 +1,24 @@
+<script setup>
+// import { useQuery } from '@tanstack/vue-query'
+
+// const { isPending, isFetching, isError, data, error } = useQuery({
+//   queryKey: ['todos'],
+//   queryFn: getTodos,
+// })
+</script>
+
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
