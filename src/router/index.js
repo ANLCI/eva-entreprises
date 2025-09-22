6db1911d9961055acba5d9b5ce70614f662c631a
Clément Prod'homme <clement.prod-homme@captive.fr>
2025-09-22 14:25:35 +0200

    ✨ Crée une évaluation lorsque l'on démarre le formulaire

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import FormView from '../views/FormView.vue'
+import AccueilView from '../views/AccueilView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
+      name: 'Accueil',
+      component: AccueilView,
+    },
+    {
+      path: '/form',
       name: 'form',
-      component: FormView,
+      component: () => import('../views/FormView.vue'),
     },
     {
       path: '/resultat',
---
+++ src/views/AccueilView.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { useMutation } from '@tanstack/vue-query'
+import { useRouter } from 'vue-router'
+import { creeEvaluation, getEvaluationParams } from './../services/evaluationService'
+
+const router = useRouter()
+
+const mutation = useMutation({
+  mutationFn: () => creeEvaluation(getEvaluationParams()),
+  onSuccess: () => {
+    router.push('/form')
+  }
+})
+
+const commencer = () => {
+  // Déclenche l'appel API lorsque le bouton est cliqué
+  mutation.mutate()
+}
+</script>
+
+<template>
+  <div class="fr-container">
+    <DsfrButton
+      label="Commencer"
+      @click="commencer"
+      primary
+      :disabled="mutation.isLoading"
+    />
+  </div>
+</template>
+
+<style>
+</style>
+
