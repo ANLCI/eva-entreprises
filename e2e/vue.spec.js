588211bd3d66095f1080e35a88a35f2486016606
Clément Prod'homme <clement.prod-homme@captive.fr>
2025-10-08 12:46:26 +0200

    ✅ Corrige les tests

--- e2e/vue.spec.js
+++ e2e/vue.spec.js
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-const mockApiResponse = [
+const mockApiQuestionnaire1 = [
   {
     "id": "c325e445-6546-4453-a90e-97150abce34e",
     "nom_technique": "Q1PG01",
@@ -59,6 +59,77 @@ const mockApiResponse = [
   }
 ];
 
+const mockApiQuestionnaire2 = [
+  {
+    "id": "727257e5-5738-4917-a1c0-54cb14807530",
+    "nom_technique": "Q2PC01",
+    "metacompetence": null,
+    "type_qcm": "standard",
+    "description": "",
+    "demarrage_audio_modalite_reponse": false,
+    "illustration": null,
+    "score": null,
+    "type": "qcm",
+    "consigne_audio": null,
+    "audio_url": null,
+    "intitule_audio": null,
+    "intitule": "Avez-vous parfois l'impression de devoir prendre plus de temps que nécessaire pour vous assurer que vos collaborateurs ou collaboratrices ont bien compris certaines informations ?",
+    "modalite_reponse": null,
+    "choix": [
+      {
+        "id": "0fb6c3c1-07ca-4422-a7b2-d2ecb08d7e5d",
+        "nom_technique": "Q2PC01R1",
+        "intitule": "Oui",
+        "type_choix": "bon",
+        "position": 1,
+        "audio_url": null
+      },
+      {
+        "id": "b07cb7f0-48ac-4ce8-b469-82a388440a6d",
+        "nom_technique": "Q2PC01R2",
+        "intitule": "Non",
+        "type_choix": "bon",
+        "position": 2,
+        "audio_url": null
+      }
+    ]
+  },
+  {
+    "id": "6a1af3c4-8548-4d80-97a3-3782e5f6f88d",
+    "nom_technique": "Q2PC02",
+    "metacompetence": null,
+    "type_qcm": "standard",
+    "description": "",
+    "demarrage_audio_modalite_reponse": false,
+    "illustration": null,
+    "score": null,
+    "type": "qcm",
+    "consigne_audio": null,
+    "audio_url": null,
+    "intitule_audio": null,
+    "intitule": "Avez-vous des collègues, chefs, managers, qui estiment devoir prendre en charge une partie du travail de leurs collaborateurs ou collaboratrices (glissement de fonctions) pour pallier les difficultés avec les compétences de base (lire, écrire, compter, cliquer) ?",
+    "modalite_reponse": null,
+    "choix": [
+      {
+        "id": "7c17bc6f-d23d-4cd9-9b96-b427d4fdefe1",
+        "nom_technique": "Q2PC02R1",
+        "intitule": "Oui",
+        "type_choix": "bon",
+        "position": 1,
+        "audio_url": null
+      },
+      {
+        "id": "afc8d864-b72e-41b7-b260-2cb58f0fd9b0",
+        "nom_technique": "Q2PC02R2",
+        "intitule": "Non",
+        "type_choix": "bon",
+        "position": 2,
+        "audio_url": null
+      }
+    ]
+  },
+]
+
 test('Complète le premier questionnaire', async ({ page }) => {
   await page.route('*/**/api/evaluations', (route) => {
     route.fulfill({
@@ -70,7 +141,7 @@ test('Complète le premier questionnaire', async ({ page }) => {
   await page.route('*/**/api/questionnaires/**', (route) => {
     route.fulfill({
       contentType: 'application/json',
-      body: JSON.stringify(mockApiResponse),
+      body: JSON.stringify(mockApiQuestionnaire1),
     });
   });
 
@@ -113,7 +184,7 @@ test('reprend le deuxième questionnaire', async ({ page }) => {
   await page.route('*/**/api/questionnaires/**', (route) => {
     route.fulfill({
       contentType: 'application/json',
-      body: JSON.stringify(mockApiResponse),
+      body: JSON.stringify(mockApiQuestionnaire2),
     });
   });
 
@@ -127,19 +198,17 @@ test('reprend le deuxième questionnaire', async ({ page }) => {
   const evaluationId = "123456"
   await page.goto(`/evaluation-impact?evaluation_id=${evaluationId}`);
 
-  await expect(page.locator('legend')).toHaveText('Quelle est la taille de votre entreprise/structure ?');
+  await expect(page.locator('legend')).toHaveText("Avez-vous parfois l'impression de devoir prendre plus de temps que nécessaire pour vous assurer que vos collaborateurs ou collaboratrices ont bien compris certaines informations ?");
 
-  const choicesList = page.locator('label');
-  await expect(choicesList.first()).toContainText('250 salariés et +');
-  await expect(choicesList.nth(1)).toContainText('50 à 249 salariés');
-
-  // Select the first choice
-  await choicesList.first().click();
+  const choicesList1 = page.locator('label');
+  await expect(choicesList1.first()).toContainText('Oui');
+  await choicesList1.first().click();
 
   await page.click('button:has-text("Continuer")');
 
-  const inputField = page.locator('input[type="text"]');
-  await inputField.fill('Finance');
+  const choicesList2 = page.locator('label');
+  await expect(choicesList2.first()).toContainText('Oui');
+  await choicesList2.first().click();
 
   await page.click('button:has-text("Valider")');
 
