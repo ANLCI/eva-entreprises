63d603fd7b24ddd062d787d5d8861189ecd972fa
Clément Prod'homme <clement.prod-homme@captive.fr>
2025-10-09 17:45:50 +0200

    🐛 Je peux accéder à l'application depuis n'importe quel path

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 /* eslint-disable no-undef */
 
 import express from 'express';
+import path from 'path';
+
 const app = express();
 
 const directory = '/' + (process.env.STATIC_DIR || 'dist');
-app.use(express.static(process.cwd() + directory));
+app.use(express.static(path.join(process.cwd(), directory)));
+
+// Redirige toutes les requêtes vers index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(process.cwd(), directory, 'index.html'));
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
